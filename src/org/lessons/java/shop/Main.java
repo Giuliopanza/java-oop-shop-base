2542fc670d41fbbb1ad785d0c87f05eadb6086f4
Giulio <giuliopanza2000@gmail.com>
2025-05-15 17:51:31 +0200

    Edit public - private

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,6 +1,6 @@
 /*
 Nel progetto java-oop-shop, package org.lessons.java.shop,
-creare la classe Prodotto che gestisce i prodotti dello shop. 
+creare la classe Prodotto che gestisce i prodotti dello shop.
 
 Un prodotto è caratterizzato da:
 - codice (numero intero)
@@ -16,7 +16,22 @@ Usate opportunamente costruttori, attributi ed eventuali altri metodi di “util
 - il prodotto esponga un metodo per avere il nome esteso, ottenuto concatenando codice-nome
 
 Nello stesso package aggiungete una classe Main con metodo main nella quale testate tutte
-le funzionalità della classe Prodotto. 
+le funzionalità della classe Prodotto.
+
+Creare la classe Prodotto che gestisce i prodotti dello shop.
+
+Un prodotto è caratterizzato da:
+- codice (numero intero)
+- nome
+- marca
+- prezzo
+- iva
+
+Usate opportunamente i livelli di accesso (public, private), i costruttori, i metodi getter e setter
+ed eventuali altri metodi di "utilità" per fare in modo che:
+- esistano almeno due costruttori diversi
+- il codice prodotto sia accessibile solo in lettura
+- gli altri attributi siano accessibili sia in lettura che in scrittura
 */
 package org.lessons.java.shop;
 
@@ -24,12 +39,13 @@ public class Main {
 
     public static void main(String[] args) {
         Prodotto Latte = new Prodotto("latte", "da bere", 6.5f, 10);
-        System.out.println(Latte.prezzotot);
-        System.out.println(Latte.nametot);
+        System.out.println(Latte.getIva() * Latte.getPrezzo() / 100 + Latte.getPrezzo());
+        System.out.println(Latte.getCode() + "-" + Latte.getName());
+        System.out.println(Latte.getCode());
         Prodotto Miele = new Prodotto("miele", "da mangiare", 6.5f, 10);
-        System.out.println(Miele.prezzotot);
-        System.out.println(Miele.nametot);
-
+        System.out.println(Miele.getIva() * Miele.getPrezzo() / 100 + Miele.getPrezzo());
+        System.out.println(Miele.getCode() + "-" + Miele.getName());
+        System.out.println(Miele.getCode());
     }
 
 }
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -3,21 +3,57 @@ package org.lessons.java.shop;
 import java.util.Random;
 
 public class Prodotto {
-    Random random = new Random();
-    public int code = random.nextInt(10);
-    public String name;
-    public String nametot;
-    public String description;
-    public float prezzo;
-    public float prezzotot;
-    public int iva;
+
+    private int code;
+    private String name;
+    private String description;
+    private float prezzo;
+    private int iva;
+    
 
     public Prodotto (String name, String description, float prezzo, int iva){
+        Random random = new Random();
+        this.code = random.nextInt(999);
         this.name = name;
-        nametot = code + "-" + name; 
         this.description = description;
         this.prezzo = prezzo;
         this.iva = iva;
-        prezzotot = iva * prezzo / 100 + prezzo;
+
+    }
+
+    public int getCode (){
+        return this.code;
+    }
+
+    public String getName (){
+        return name;
+    }
+
+    public void setName (String name){
+        this.name=name;
+    }
+
+    public String getDescription (){
+        return description;
+    }
+
+    public void setDescription (String description){
+        this.description=description;
+    }
+
+    public float getPrezzo (){
+        return prezzo;
+    }
+
+    public void setPrezzo (float prezzo){
+        this.prezzo=prezzo;
+    }
+
+    public int getIva (){
+        return iva;
+    }
+
+    public void setIva (int iva){
+        this.iva=iva;
     }
 }
